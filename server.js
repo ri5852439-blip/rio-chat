864443cc0a462d3b0a539426b086dc6d1dcb0d90
Rifat <YOUR_EMAILgit config --global user.email rifar7234r@gmail.com>
2026-07-13 22:21:40 -0800

    Fix seen feature

--- server.js
+++ server.js
@@ -8,12 +8,36 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
+let onlineUsers = 0;
+
 io.on("connection", (socket) => {
+
+  onlineUsers++;
+  io.emit("online count", onlineUsers);
+
   console.log("User connected");
 
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
+
+  socket.on("typing", (name) => {
+    socket.broadcast.emit("typing", name);
+  });
+
+  socket.on("stop typing", () => {
+    socket.broadcast.emit("stop typing");
+  });
+
+  socket.on("seen all", () => {
+    socket.broadcast.emit("messages seen");
+  });
+
+  socket.on("disconnect", () => {
+    onlineUsers--;
+    io.emit("online count", onlineUsers);
+  });
+
 });
 
 const PORT = process.env.PORT || 3000;
